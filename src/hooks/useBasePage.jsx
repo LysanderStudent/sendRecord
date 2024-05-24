7bf6b80f380e0ed0c41857c74bdc942fdc12417a
Lysander-ayudate <Lysander.Paniagua@ayudatelta.com>
2024-05-24 14:44:39 -0600

    inport useEffect

--- src/hooks/useBasePage.jsx
+++ src/hooks/useBasePage.jsx
@@ -1,5 +1,5 @@
 import { message } from 'antd';
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const useBasePage = () => {
     const projectName = "[Nombre del proyecto]";
